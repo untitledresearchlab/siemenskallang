f84de7818bde31fa51c6e3680f2811b231682564
untitledresearchlab <untitledrabbithole@gmail.com>
2023-09-11 11:35:51 +0800

    Add existing project files prior to the push to GitHub.

---
+++ index.js
@@ -0,0 +1,348 @@
+﻿/*
+
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠤⢤⣄⠀⢀⣤⠤⠤⠤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⠞⠉⠀⠀⠀⠈⣻⠋⠀⠀⠀⠀⠀⠉⠻⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⣯⡴⠖⠲⠦⣤⡴⠛⠉⠉⠉⠙⠒⢦⣄⠀⠈⠻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠞⠋⠀⠀⠀⠀⣠⢏⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣠⣾⣿⣿⣿⣿⣄⠀⢰⢟⣩⣿⡿⠿⠿⠿⠿⠿⣷⣆⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣹⣿⠿⣾⣷⣿⣯⣡⣿⣿⠟⠛⣷⣶⣶⣶⣶⣶⡾⠿⠃⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡶⠷⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠻⣷⣤⣿⣾⢿⣿⣿⣿⣯⣀⣀⣿⣿⣿⣛⣩⠾⠃⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⡿⢿⡇⠐⣶⠞⠋⠉⠻⣶⠒⠒⠋⠉⠉⠁⠀⠀⢠⣄⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⢹⡀⠀⠀⠀⠀⠀⠀
+⢰⣿⡀⠀⠀⠞⠁⠀⠀⠀⠀⠈⠙⠶⢤⡀⠀⠀⠀⣠⣶⣶⣧⠀⠀⠀⠀⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀
+⠀⠻⣷⣤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⠴⠖⣺⣿⣫⢷⣳⠿⠁⠀⠀⠀⠀⠘⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⢸⣃⣼⠁⠀⠀⠀⠀⠀⠀
+⠀⠸⣏⠛⠖⠒⠒⠒⠒⠒⠚⠚⠛⠛⠛⣋⣡⠶⠚⠋⠀⠀⠀⠀⠀⠀⠀⠀⠻⣧⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⣾⠇⠀⠀⠈⣽⡟⣠⣾⣶⡄⠀⠀⠀
+⠀⠀⢹⡗⠶⠤⠤⠤⠤⠴⠤⠶⠖⠒⠚⠉⠁⠀⠀⠀⠀⠀⠀⣷⠀⢻⡄⠀⠀⢹⣷⠀⠀⠀⣀⣴⠶⠟⠛⠛⣻⡟⠀⠀⠀⠰⢿⠞⣛⣷⣞⣇⡀⠀⠀
+⠀⠀⠈⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠈⣿⡀⠀⠀⣻⣧⣠⡾⠻⣥⣀⠀⣀⣴⣿⣦⣶⣶⣶⣬⣍⡈⠙⠛⠛⠛⠛⣷⡀
+⠀⠀⠀⠀⠙⢷⡀⠀⠀⠀⠀HELLO⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⢹⣇⠀⠀⠀⢿⡟⠀⣀⣈⣹⡿⠟⠁⠀⠀⠀⠀⠀⠀⠉⠻⣦⢀⡀⢰⣤⣿⠃
+⠀⠀⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀THERE⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⢿⠀⠀⠀⠀⠻⠆⠀⣼⡟⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠹⣷⠿⠞⠛⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠛⢷⣄⠀⠀⠀THIS⠀⠀⠀⠀⠀⠀⠀⢹⡄⢸⣇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠴⠟⠀⠀⠀⠀⠀⠀⠀⠀⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⡀⠀⠀IS THE⠀⠀⠀⠀⠀⠈⣿⡈⢿⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀SIEMENS⠀⠀⠀⠀⠀⢀⣾⣇⠰⣷⣤⣭⣿⠆⠀⠀⠀⠀⠀⣶⣟⢠⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣄⠀⠀APP⠀⠀⠀⠀⠀⢸⣏⢸⣧⣽⡅⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠋⢀⣀⠀⠀⣠⣾⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⣀⣼⡿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣤⣤⣴⡾⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣧⠀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠷⣶⣦⣤⣤⣤⣤⣤⣤⣶⣶⡶⠿⠛⠉⠀⢻⡄⠸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⣿⡀⠀⠀⠀⠀⣰⡶⠶⠶⣾⣿⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⣿⠀⠀⠀⠀⠀⠘⠿⣦⣄⡀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣄⡀⠀⣿⠀⣿⠁⠀⠀⠀⠀⠀⠀⠈⠉⠛⠳⠶⣶⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⡉⠛⢿⣿⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣶⣄⣁⣸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+IP ADDRESS LIST:
+ROUTER : 192.168.0.1
+NODEJS SERVER : 192.168.0.2 / 	48-21-0B-59-24-5A
+WATCHOUT : 192.168.0.3 / B4-2E-99-F8-16-47
+MADMAPPER : 192.168.0.4 / 48-21-0B-3B-64-9B
+CAPTURE CARD 1 : 192.168.0.5 / D0-C8-57-81-01-9F
+CAPTURE CARD 2 : 192.168.0.6 / D0-C8-57-81-01-A0
+
+
+*/
+
+
+var udp = require('dgram');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9000;
+var cors = require('cors')
+const cron = require("node-cron");
+var wintools = require('wintools');
+const axios = require('axios');
+const osc = require('osc');
+var client = udp.createSocket('udp4');
+
+//WO port and IP
+var PORT = 3039;
+var HOST = '192.168.0.3';
+
+
+const oscPort = new osc.UDPPort({ localAddress: '0.0.0.0' });
+oscPort.open({ port: 8000 });
+
+app.listen(port, () => {
+    console.log(`Node  app listening on port ${port}`)
+    console.log('OSC sending on port 8000')
+})
+
+
+var whitelist = ['http://localhost']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors())
+app.options(corsOptions, cors());
+
+//--------------------- System Control -------------------------------------------//
+
+//Function to turn on entire network
+
+app.get('/wakesys', wakeSystem);
+
+function wakeSystem() {
+    var wolwo = require('wake_on_lan');
+    var wolmad = require('wake_on_lan');
+
+    wolwo.wake('B42E99F81647');
+    wolmad.wake('48210B3B649B');
+
+    wolwo.wake('B42E99F81647', function (error) {
+        if (error) {
+            // handle error
+        } else {
+            // done sending packets
+        }
+    });
+    // B42E99F81647
+
+
+    wolmad.wake('48210B3B649B', function (error) {
+        if (error) {
+            // handle error
+        } else {
+            // done sending packets
+        }
+    });
+   // 48210B3B649B
+
+
+}
+
+//Function to turn off entire network
+
+app.get('/shutsys', shutsys);
+
+function shutsys() {
+
+   
+
+    //Watchout
+    client.send(shutdownwo, 0, shutdownwo.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        //res.send('Playing presentation video');
+    });
+
+    //MADMAPPER
+    axios.get('http://192.168.0.4:3000/trigger')
+        .then(response => {
+            console.log(response.data.url);
+            console.log(response.data.explanation);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+}
+
+//Function to turn on Watchout
+
+app.get('/wakeWO', wakeWO);
+
+function wakeWO() {
+
+    var wolwo = require('wake_on_lan');
+
+    wolwo.wake('B42E99F81647');
+    
+
+    wolwo.wake('B42E99F81647', function (error) {
+        if (error) {
+            // handle error
+        } else {
+            // done sending packets
+        }
+    });
+    // 48-21-0B-33-6B-AE
+
+
+
+}
+
+//Function to turn off Watchout
+
+app.get('/shutWO', shutWO);
+
+/*
+function shutWO() {
+
+    //Watchout
+    axios.get('http://192.168.0.3:6969/shutsys')
+        .then(response => {
+            console.log(response.data.url);
+            console.log(response.data.explanation);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+} */
+var shutdownwo = Buffer.from("authenticate 1  \npowerDown");
+function shutWO() {
+
+    //Watchout
+    client.send(shutdownwo, 0, shutdownwo.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        //res.send('Playing presentation video');
+    });
+
+}
+
+//Function to turn on Madmapper
+
+app.get('/wakeMad', wakeMad);
+
+function wakeMad() {
+
+    var wolmad = require('wake_on_lan');
+
+    wolmad.wake('48210B354C54');
+
+    wolmad.wake('48210B354C54', function (error) {
+        if (error) {
+            // handle error
+        } else {
+            // done sending packets
+        }
+    });
+   // 48 - 21 - 0B - 35 - 4C - 54
+
+
+
+}
+
+//Function to turn off Madmapper
+
+app.get('/shutMad', shutMad);
+
+function shutMad() {
+
+    //MADMAPPER
+    axios.get('http://192.168.0.4:6969/shutsys')
+        .then(response => {
+            console.log(response.data.url);
+            console.log(response.data.explanation);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+}
+
+
+
+
+//---------------------- WO display controls ------------------------------------//
+
+app.get('/presentation', presentation);
+
+var presentation = Buffer.from("authenticate 1  \nreset \ndelay \"10\" \nrun \"presentation\"");
+
+function presentation() {
+    client.send(presentation, 0, presentation.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        //res.send('Playing presentation video');
+    });
+}
+
+app.get('/capture1', capture1);
+
+var capture1 = Buffer.from("authenticate 1  \nreset \ndelay \"10\" \nrun \"capture1\"");
+
+function capture1() {
+    client.send(capture1, 0, capture1.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        //res.send('Playing capture 1');
+    });
+}
+
+app.get('/capture2', capture2);
+
+var capture2 = Buffer.from("authenticate 1  \nreset \ndelay \"10\" \nrun \"capture2\"");
+
+function capture2() {
+    client.send(capture2, 0, capture2.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        //res.send('Playing capture 2');
+    });
+}
+
+
+
+//---------------------- Madmapper display controls ------------------------------------//
+
+app.get('/playMadcapture', playMadcapture);
+app.get('/playLoopvideo', playLoopvideo);
+
+function playMadcapture(req, res) {
+
+    oscPort.send({
+        timeTag: osc.timeTag(0),
+        packets: [{ address: '/cues/selected/columns/1' },
+        ],
+    }, '192.168.1.202', 9000);
+    // client.close();
+    res.send('Playing Zone 2 Presentation Content');
+    console.log('Playing Zone 2 Presentation Content');
+}
+
+function playLoopvideo(req, res) {
+
+    oscPort.send({
+        timeTag: osc.timeTag(0),
+        packets: [{ address: '/cues/selected/columns/1' },
+        ],
+    }, '192.168.0.4', 8000);
+    // client.close();
+    res.send('Playing Zone 2 Loop Content');
+    console.log('Playing Zone 2 Loop Content');
+}
+
+
+
+
+
+
+/*
+ ⢀⠔⠂⠉⠉⠉⠉⠑⠢⡄⣀⠔⠊⠁⠀⠒⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⡰⠁⠀⠀⠀⠀⢀⣀⣀⣀⠘⡇⠀⠀⠀⠀⠀⠀⠘⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠁⠀⠀⣠⠔⠉⠁⠀⠀⠀⠉⠉⠲⣤⠖⠒⠒⠒⠲⠬⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠔⣊⠭⠭⠭⠭⢭⣛⠩⣉⠲⣇⠀⠀⢀⣶⠶⢦⣶⢷⣤⡀⠀⠀⠀⠀
+⠀⠀⢸⣵⣉⡤⠤⢤⣤⣤⣤⣬⠵⠮⣶⣌⣇⠐⣫⣶⣭⣭⣍⡑⢼⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⣅⠲⣿⣞⣿⣉⣿⠀⠀⠘⡎⣇⡜⣿⣾⣿⣹⡇⠈⣽⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠓⠤⢈⣉⣛⣓⣂⣒⣊⡽⠂⢹⣛⠛⢛⠛⣒⣩⠞⠀⠀⠀⠀⠀
+⠀⠠⠤⠂⠀⠀⠀⠀⠀⠀⠀⠀⣀⠼⠁⠀⠀⠀⠈⠫⡁⠐⠛⠁⠱⡀⠀⠀⠀⠀
+⢠⢶⠭⠭⣄⣀⠀⠀⠀⠤⠒⠉⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⢰⢻⠄⠀⠀⠀
+⠀⠘⢦⣙⠲⠤⣍⡉⠑⠒⠢⠤⠤⠤⣀⣀⣀⣀⣀⣀⣀⣀⣀⠔⣡⠊⠀⠀⠀⠀
+⠀⠀⠀⠈⠉⠢⢄⡈⠉⠁⠀⠀⠒⠒⠦⠤⠤⠤⠤⠤⠤⠤⠤⠊⡸⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠂⠤⠤⠤⠤⢄⣀⣀⡤⠤⠤⢤⠤⠚⠅⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠤⠒⠓⢤⠞⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⢄⡠⢶⡁⠀⣀⣀⡀⢑⠢⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠣⢶⠕⢋⢔⡵⠗⠁⠀⠈⠳⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠘⣖⡝⠁⠀⢀⠔⠀⠀⠀⠘⣆⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠈⠑⠤⢠⠃⠀⣠⠞⢀⠄⠈⢆
+▀█▀ █░█ █▀▀ █▀ █░█ █▀█ █▀▀ █▀█ █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█
+░█░ █▀█ ██▄ ▄█ █▄█ █▀▀ ██▄ █▀▄ ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█
+*/
---
+++ my-app/src/App.js
@@ -0,0 +1,258 @@
+
+import './App.css';
+
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { Grid } from '@mui/material';
+
+const SystemButton = () => {
+
+    return (
+
+
+
+
+
+        <Grid container justifycontent="center">
+            <Grid item>
+               
+
+                    
+                        <h1>Siemens System Control</h1>
+                        
+                            <h3 >Entire Network</h3>
+                            <ButtonGroup variant="contained"
+                                aria-label="outlined primary button group"
+                                justifycontent="center">
+                                
+
+                                <Button onClick={onsystem}>TURN ON SYSTEM</Button>
+                                <Button onClick={offsystem}>TURN OFF SYSTEM</Button>
+
+                            </ButtonGroup>
+                        
+                       
+
+                        
+
+                            <h3 >Watchout Control</h3>
+                            <ButtonGroup variant="contained"
+                                aria-label="outlined primary button group"
+                                justifycontent="center">
+                                
+
+                                <Button onClick={wakewo}>TURN ON WATCHOUT</Button>
+                                <Button onClick={shutwo}>TURN OFF WATCHOUT</Button>
+
+                            </ButtonGroup>
+
+                            
+
+                                <h3 >Watchout Playback</h3>
+                                <ButtonGroup variant="contained"
+                                    aria-label="outlined primary button group"
+                                    justifycontent="center">
+                                    
+
+                                    <Button onClick={presentation}>Play Presentation</Button>
+                                    <Button onClick={capture1}>Play Capture</Button>
+
+                                </ButtonGroup>
+
+
+                            
+                        
+                        
+
+                            <h3 >TV Control</h3>
+                            <ButtonGroup variant="contained"
+                                aria-label="outlined primary button group"
+                                justifycontent="center">
+                                
+
+                                <Button onClick={wakeMad}>TURN ON TV</Button>
+                                <Button onClick={shutMad}>TURN OFF TV</Button>
+
+                            </ButtonGroup>
+
+                           
+
+                                <h3 >TV Playback</h3>
+                                <ButtonGroup variant="contained"
+                                    aria-label="outlined primary button group"
+                                    justifycontent="center">
+                                    
+
+                                    <Button onClick={playLoopvideo}>Play Loop</Button>
+                                    <Button onClick={playMadcapture}>Play Presentation</Button>
+
+                                </ButtonGroup>
+
+
+                           
+                       
+
+
+                    
+
+
+
+                
+            </Grid>
+        </Grid>
+
+    );
+
+}
+
+//---- Call Functions ----//
+
+function onsystem() {
+    fetch('http://192.168.1.128:9000/wakesys')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function offsystem() {
+    fetch('http://192.168.1.128:9000/shutSystem')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function wakewo() {
+    fetch('http://192.168.1.128:9000/wakeWO')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function shutwo() {
+    fetch('http://192.168.1.128:9000/shutWO')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function wakeMad() {
+    fetch('http://192.168.1.128:9000/wakeMad')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function shutMad() {
+    fetch('http://192.168.1.128:9000/shutMad')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+
+
+function presentation() {
+    fetch('http://192.168.1.128:9000/presentation')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function capture1() {
+    fetch('http://192.168.0.2:9000/capture1')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function playMadcapture() {
+    fetch('http://192.168.1.128:9000/playMadcapture')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+function playLoopvideo() {
+    fetch('http://192.168.1.128:9000/playLoopvideo')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
+//-----------------------//.
+
+function App() {
+  return (
+      <div>
+
+
+          
+
+
+              <SystemButton />
+
+          
+
+
+
+
+
+
+
+
+      </div>
+
+  );
+}
+
+export default App;
