5d958ed9e895b4c813882e8aaa4443788bee2bbb
untitledresearchlab <86580678+untitledresearchlab@users.noreply.github.com>
2023-09-12 11:46:01 +0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
 
 import './App.css';
-
+import logo from './logo_resized_resized.png'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { Grid } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#009999',
+            // light: will be calculated from palette.primary.main,
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+    }
+});
 
 const SystemButton = () => {
 
@@ -18,84 +31,68 @@ const SystemButton = () => {
                 transform: "translate(-50%, -50%)",
             }}
         >
-
-
+           
+        
 
         <Grid container>
             <Grid item>
-               
+                    <img src={logo} />
 
-                    
-                        <h1 >Siemens System Control</h1>
-                        
-                            <h3 >Entire Network</h3>
-                            <ButtonGroup variant="contained"
-                                aria-label="outlined primary button group"
-                                >
+                    <h3 >Entire Network</h3>
+                    <h4 >*This setting will toggle both TV Wall and Individual TVs</h4>
+                    <ThemeProvider theme={theme}>
+                    <ButtonGroup variant="contained" color="primary" aria-label="outlined primary button group">
                                 
 
                                 <Button onClick={onsystem}>TURN ON SYSTEM</Button>
                                 <Button onClick={offsystem}>TURN OFF SYSTEM</Button>
 
-                            </ButtonGroup>
-                        
-                       
-
+                        </ButtonGroup>
+                            </ThemeProvider>
                         
 
-                            <h3 >Watchout Control</h3>
-                            <ButtonGroup variant="contained"
-                                aria-label="outlined primary button group"
-                                justifycontent="center">
+                    <h3 >TV Wall Control</h3>
+                    <ThemeProvider theme={theme}>
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group">
                                 
 
-                                <Button onClick={wakewo}>TURN ON WATCHOUT</Button>
-                                <Button onClick={shutwo}>TURN OFF WATCHOUT</Button>
+                             <Button onClick={wakewo}>TURN ON WATCHOUT</Button>
+                             <Button onClick={shutwo}>TURN OFF WATCHOUT</Button>
 
                             </ButtonGroup>
+                    </ThemeProvider>
 
-                            
-
-                                <h3 >Watchout Playback</h3>
-                                <ButtonGroup variant="contained"
-                                    aria-label="outlined primary button group"
-                                    justifycontent="center">
+                    <h3 >TV Wall Playback</h3>
+                    <ThemeProvider theme={theme}>
+                                <ButtonGroup variant="contained" aria-label="outlined primary button group" justifycontent="center">
                                     
-
-                                    <Button onClick={presentation}>Play Presentation</Button>
-                                    <Button onClick={capture1}>Play Capture</Button>
+                                 <Button onClick={presentation}>Play Presentation</Button>
+                                 <Button onClick={capture1}>Right Stage Capture</Button>
+                                 <Button onClick={capture2}>Left Stage Capture</Button>
 
                                 </ButtonGroup>
+                    </ThemeProvider>
 
-
-                            
-                        
-                        
-
-                            <h3 >TV Control</h3>
-                            <ButtonGroup variant="contained"
-                                aria-label="outlined primary button group"
-                                justifycontent="center">
-                                
+                    <h3 >TV Control</h3>
+                    <ThemeProvider theme={theme}>
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group" justifycontent="center">
 
                                 <Button onClick={wakeMad}>TURN ON TV</Button>
                                 <Button onClick={shutMad}>TURN OFF TV</Button>
 
                             </ButtonGroup>
-
+                    </ThemeProvider>
                            
 
-                                <h3 >TV Playback</h3>
-                                <ButtonGroup variant="contained"
-                                    aria-label="outlined primary button group"
-                                    justifycontent="center">
+                    <h3 >TV Playback</h3>
+                    <ThemeProvider theme={theme}>
+                                <ButtonGroup variant="contained" aria-label="outlined primary button group" justifycontent="center">
                                     
-
                                     <Button onClick={playLoopvideo}>Play Loop</Button>
                                     <Button onClick={playMadcapture}>Play Presentation</Button>
 
                                 </ButtonGroup>
-
+                    </ThemeProvider>
 
                            
                        
@@ -116,7 +113,7 @@ const SystemButton = () => {
 //---- Call Functions ----//
 
 function onsystem() {
-    fetch('http://192.168.1.128:9000/wakesys')
+    fetch('http://192.168.0.2:9000/wakesys')
         .then((response) => {
             return response.text();
         })
@@ -128,7 +125,7 @@ function onsystem() {
 }
 
 function offsystem() {
-    fetch('http://192.168.1.128:9000/shutSystem')
+    fetch('http://192.168.0.2:9000/shutSystem')
         .then((response) => {
             return response.text();
         })
@@ -140,7 +137,7 @@ function offsystem() {
 }
 
 function wakewo() {
-    fetch('http://192.168.1.128:9000/wakeWO')
+    fetch('http://192.168.0.2:9000/wakeWO')
         .then((response) => {
             return response.text();
         })
@@ -152,7 +149,7 @@ function wakewo() {
 }
 
 function shutwo() {
-    fetch('http://192.168.1.128:9000/shutWO')
+    fetch('http://192.168.0.2:9000/shutWO')
         .then((response) => {
             return response.text();
         })
@@ -164,7 +161,7 @@ function shutwo() {
 }
 
 function wakeMad() {
-    fetch('http://192.168.1.128:9000/wakeMad')
+    fetch('http://192.168.0.2:9000/wakeMad')
         .then((response) => {
             return response.text();
         })
@@ -176,7 +173,7 @@ function wakeMad() {
 }
 
 function shutMad() {
-    fetch('http://192.168.1.128:9000/shutMad')
+    fetch('http://192.168.0.2:9000/shutMad')
         .then((response) => {
             return response.text();
         })
@@ -190,7 +187,7 @@ function shutMad() {
 
 
 function presentation() {
-    fetch('http://192.168.1.128:9000/presentation')
+    fetch('http://192.168.0.2:9000/presentation')
         .then((response) => {
             return response.text();
         })
@@ -213,8 +210,20 @@ function capture1() {
 
 }
 
+function capture2() {
+    fetch('http://192.168.0.2:9000/capture2')
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+
+}
+
 function playMadcapture() {
-    fetch('http://192.168.1.128:9000/playMadcapture')
+    fetch('http://192.168.0.2:9000/playMadcapture')
         .then((response) => {
             return response.text();
         })
@@ -226,7 +235,7 @@ function playMadcapture() {
 }
 
 function playLoopvideo() {
-    fetch('http://192.168.1.128:9000/playLoopvideo')
+    fetch('http://192.168.0.2:9000/playLoopvideo')
         .then((response) => {
             return response.text();
         })
@@ -242,20 +251,8 @@ function playLoopvideo() {
 function App() {
   return (
       <div>
-
-
-          
-
-
+            
               <SystemButton />
-
-          
-
-
-
-
-
-
 
 
       </div>
